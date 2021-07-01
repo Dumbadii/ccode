f65db9713ffa4ea3df40c381ab7fc346905c4f25
yyc-think <yeyuchao@gmail.com>
2021-07-01 14:31:34 +0800

    exe123.c remove comments

---
+++ exe123.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MAXLINE 80
+#define MAXFILE 1000
+
+int getlinex(char line[], int maxline);
+int appendline(char fi[], char line[], int maxfile);
+void removecomments(char fi[], int maxfile);
+
+int main() {
+    char fi[MAXFILE];
+    char newfi[MAXFILE];
+    char line[MAXLINE];
+
+
+}
