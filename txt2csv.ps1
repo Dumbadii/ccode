0b30aadae2aaefc5eacb775dbd2f591b8504b328
yeyuc <yeyuchao@gmail.com>
2021-10-01 13:14:33 +0800

    updata 2csv

--- txt2csv.ps1
+++ txt2csv.ps1
@@ -1,17 +1,22 @@
 function tocsv($file)
 {
     $target = $file -replace "(\w+)\..*",'$1.csv';
-    $con = Get-Content $file;
-
-    $head = $con -replace ".*'data':\[\{([^\}]*)\},.*", '$1';
+    $data = (Get-Content $file) -match "'data':(\[(.*)\])";
+    
+    if($data.Length -eq 0) {write-host "not found";return;}
+    
+    $head = $data[0] -replace "'data':\[\{([^\}]*)\},.*", '$1';
     $head = $head -replace "'(\w*)':'\w*'", '$1';
     Set-Content $target $head;
 
-    $con = $con -replace ".*'data':\[\{(.*)\}\].*", '$1';
-    $con = $con -replace "},{", "`r`n";
-    $con = $con -replace "'\w*':'(\w*)'", '$1';
+    foreach($con in $data) {
+        $con = $con -replace "'data':\[\{(.*)\}\]", '$1';
+        $con = $con -replace "},{", "`r`n";
+        $con = $con -replace "'\w*':'(\w*)'", '$1';
     
-    Add-Content $target $con;
+        Add-Content $target $con;
+    }
+
 }
 
 $files = (Get-ChildItem -name *.txt);
