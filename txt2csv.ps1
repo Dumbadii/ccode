4a771c3ddf9cf49aa598898f3f22eae6d5bb8e7d
yeyuc <yeyuchao@gmail.com>
2021-10-01 13:50:10 +0800

    update t2c

--- txt2csv.ps1
+++ txt2csv.ps1
@@ -1,16 +1,16 @@
 function tocsv($file)
 {
     $target = $file -replace "(\w+)\..*",'$1.csv';
-    $data = (Get-Content $file) -match "'data':(\[(.*)\])";
+    $data = (Get-Content $file) -match "'(data|result)'\s*:\s*(\[(.*)\])";
     
     if($data.Length -eq 0) {write-host "not found";return;}
     
-    $head = $data[0] -replace "'data':\[\{([^\}]*)\},.*", '$1';
+    $head = $data[0] -replace "'(data|result)'\s*:\s*\[\{([^\}]*)\},.*", '$2';
     $head = $head -replace "'(\w*)':'\w*'", '$1';
     Set-Content $target $head;
 
     foreach($con in $data) {
-        $con = $con -replace "'data':\[\{(.*)\}\]", '$1';
+        $con = $con -replace "'(data|result)'\s*:\s*\[\{(.*)\}\]", '$2';
         $con = $con -replace "},{", "`r`n";
         $con = $con -replace "'\w*':'(\w*)'", '$1';
     
